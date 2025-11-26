49e45bd73ebc8d71cbedaf0dbf64fb6d0e7f4cbd
johnasblasco <johnaslblasco@gmail.com>
2025-11-26 22:34:50 +0800

    first commit

--- src/api/AuthApi.ts
+++ src/api/AuthApi.ts
@@ -9,3 +9,7 @@ export const logout = async () => {
     const response = await axios.post('/logout')
     return response.data
 }
+
+//HOW TO USE !
+// 1. import { login, logout } from "./AuthApi";
+// 2. const res = await login(username, password);
